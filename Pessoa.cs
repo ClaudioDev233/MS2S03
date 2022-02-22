7d41c8a400359f2e24c09ff4cc3d9299bdeae4b8
Claudio <claudio.junior233@gmail.com>
2022-02-22 11:59:22 -0300

    Adicionar arquivos de projeto.

---
+++ Pessoa.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class Class1
+{
+	public Pessoa()
+	{
+		int private _nascimento;
+		int private _altura;
+
+		string private _nome;
+
+		public int Altura{ get; set; };
+		public int Nascimento { get; set; };
+	public string Nome { get; set; };	
+
+	public ShowPerson()
+    {
+		return ($"Nome: {_nome} , Idade {_nascimento - 2022} , altura {_altura} ")
+    }
+}
+}
